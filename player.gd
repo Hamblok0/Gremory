a1452d9b9673791126b4a829fda305998105e94a
Joseph Cooper <hambloko@gmail.com>
2024-03-26 17:32:33 -0500

    Forgot to stage all changes

--- player.gd
+++ player.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+class_name Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,7 +9,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
-
-func _render_player() -> void: 
 	pass
