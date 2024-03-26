0e5a89a885addc3805d38923c35b2dfd32147675
Joseph Cooper <hambloko@gmail.com>
2024-03-26 17:31:35 -0500

    New tileset resource, player rendering - Moved the tilemap's tileset to its own resource - Player now spawns on the map

--- game.gd
+++ game.gd
@@ -1,11 +1,18 @@
 extends Node2D
 
-
+var player: PackedScene = preload("res://player.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	spawn_player()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func spawn_player() -> void:
+	var new_player: Player = player.instantiate()
+	var tilemap: TileMap = $Tiles
+
+	add_child(new_player)	
+	tilemap.set_cell(4, Vector2i(7, 0), 0, Vector2i(1, 3))
+	
