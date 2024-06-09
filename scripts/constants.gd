f7b3ad181598c1b37682ca1a5eeeb50ebf1d0544
Joseph Cooper <hambloko@gmail.com>
2024-06-08 19:24:53 -0500

    BSP updates
    - Added new constants for different atlas coords on tilemap
    - Erased test map for first iteration of BSP dungeon gen
    - Currently only renders floors with no gaps

--- scripts/constants.gd
+++ scripts/constants.gd
@@ -11,5 +11,9 @@ const layers: Dictionary = {
 }
 
 const atlas: Dictionary = {
+	"wall1": Vector2i(0, 2),
+	"wall2": Vector2i(1, 2),
+	"floor1": Vector2i(8, 2),
+	"closed_door": Vector2i(13, 2), 
     "opened_door": Vector2i(14, 2)
 }
--- scripts/dungeon_creator.gd
+++ scripts/dungeon_creator.gd
@@ -5,8 +5,10 @@ extends Node2D
 @export var room_min: Vector2i = Vector2(5, 5)
 
 var root_node: Leaf
+var tilemap: TileMap
 
 func _ready() -> void:
+	tilemap = get_node("../Tiles")
 	root_node = Leaf.new(Vector2i(0, 0), Vector2i(60, 30))
 	root_node.split(5)
 	queue_redraw()
@@ -14,15 +16,8 @@ func _ready() -> void:
 
 func _draw() -> void:
 	for leaf: Leaf in root_node.get_leaves():
-		draw_rect(
-			Rect2(
-				leaf.position.x * Constants.TILE_SIZE,
-				leaf.position.y * Constants.TILE_SIZE,
-				leaf.size.x * Constants.TILE_SIZE,
-				leaf.size.y * Constants.TILE_SIZE
-			),
-			Color.GREEN,
-			false
-		)
+		for x in range(leaf.size.x):
+			for y in range(leaf.size.y):
+				tilemap.set_cell(0, Vector2i(x + leaf.size.x, y + leaf.size.y), 0, Constants.atlas.floor1)
 	pass
 
