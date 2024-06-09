90e7160bacd8e78a6c35bd678d395a2fd6309487
Joseph Cooper <hambloko@gmail.com>
2024-06-08 20:21:30 -0500

    BSP updates
    - Created a debug camera to make it easier to check dungeon gen results
    - Added a function to create padding, but something's broken  and the tiles aren't being set correctly somewhere

--- scripts/dungeon_creator.gd
+++ scripts/dungeon_creator.gd
@@ -9,15 +9,36 @@ var tilemap: TileMap
 
 func _ready() -> void:
 	tilemap = get_node("../Tiles")
-	root_node = Leaf.new(Vector2i(0, 0), Vector2i(60, 30))
+	root_node = Leaf.new(Vector2i(0, 0), Vector2i(150, 150))
 	root_node.split(5)
 	queue_redraw()
 	pass
 
 func _draw() -> void:
+	var rng: RandomNumberGenerator = RandomNumberGenerator.new()
+
 	for leaf: Leaf in root_node.get_leaves():
+		var padding: Vector4i = Vector4i(
+			rng.randi_range(2,3),
+			rng.randi_range(2,3),
+			rng.randi_range(2,3),
+			rng.randi_range(2,3)
+		)
+		draw_rect(
+            Rect2(
+                leaf.position.x * Constants.TILE_SIZE,
+                leaf.position.y * Constants.TILE_SIZE,
+                leaf.size.x * Constants.TILE_SIZE,
+                leaf.size.y * Constants.TILE_SIZE
+            ), 
+            Color.GREEN, # colour
+            false # is filled
+        )
 		for x in range(leaf.size.x):
 			for y in range(leaf.size.y):
-				tilemap.set_cell(0, Vector2i(x + leaf.size.x, y + leaf.size.y), 0, Constants.atlas.floor1)
+				if not is_inside_padding(x, y, leaf, padding):
+					tilemap.set_cell(0, Vector2i(x + leaf.size.x, y + leaf.size.y), 0, Constants.atlas.floor1)
 	pass
 
+func is_inside_padding(x: int, y: int, leaf: Leaf, padding: Vector4i) -> bool:
+	return x <= padding.x or y <= padding.y or x >= leaf.size.x - padding.z or y >= leaf.size.y - padding.w
