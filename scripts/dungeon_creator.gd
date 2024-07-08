da0ff4586623afa1eb5e793d3c2214ab7491f5c1
Hambloko <hambloko@gmail.com>
2024-07-07 20:04:06 -0500

    Added walls in map gen
    - Racked my brain endlessly trying to figure out a clean way to add walls along all leaf's and corridors, realized it would just be easier to fill the map with walls and then just clear the cells wherever floors need to be placed
    - This is not going to work if I end up making or finding wall textures that give better perspective (ie, vertical walls are just the tops of walls and horizontal walls have an illusion of depth) but whatever, will worry about that later

--- scripts/dungeon_creator.gd
+++ scripts/dungeon_creator.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@export var map_size: Vector2i = Vector2(500, 500)
+@export var map_size: Vector2i = Vector2i(150, 150)
 @export var room_max: Vector2i = Vector2(20, 20)
 @export var room_min: Vector2i = Vector2(5, 5)
 
@@ -10,7 +10,11 @@ var paths: Array = []
 
 func _ready() -> void:
 	tilemap = get_node("../Tiles")
-	root_node = Leaf.new(Vector2i(0, 0), Vector2i(150, 150))
+	for x in range(map_size.x):
+		for y in range(map_size.y):
+			tilemap.set_cell(1, Vector2i(x, y), 0,Constants.atlas.wall1)
+
+	root_node = Leaf.new(Vector2i(0, 0), map_size)
 	root_node.split(5, paths)
 	queue_redraw()
 	pass
@@ -36,19 +40,25 @@ func _draw() -> void:
         #     Color.GREEN, # colour
         #     false # is filled
         # )
-		
+
 		for x in range(leaf.size.x):
 			for y in range(leaf.size.y):
 				if not is_inside_padding(x, y, leaf, padding):
-					tilemap.set_cell(0, Vector2i(x + (leaf.position.x), y + leaf.position.y), 0, Constants.atlas.floor1)
+					var cell_pos: Vector2i = Vector2i(x + (leaf.position.x), y + leaf.position.y)
+					tilemap.erase_cell(1, cell_pos)
+					tilemap.set_cell(0, cell_pos, 0, Constants.atlas.floor1)
 
 		for path: Dictionary in paths:
 			if path['left'].y == path['right'].y:
 				for i in range(path['right'].x - path['left'].x):
-					tilemap.set_cell(0, Vector2i(path['left'].x as int + i, path['left'].y as int), 0, Constants.atlas.floor1)
+					var cell_pos: Vector2i = Vector2i(path['left'].x as int + i, path['left'].y as int)
+					tilemap.erase_cell(1, cell_pos)
+					tilemap.set_cell(0, cell_pos, 0, Constants.atlas.floor1)
 			else:
 				for i in range(path['right'].y - path['left'].y):
-					tilemap.set_cell(0, Vector2i(path['left'].x as int, path['left'].y as int + i), 0, Constants.atlas.floor1)
+					var cell_pos: Vector2i = Vector2i(path['left'].x as int, path['left'].y as int + i)
+					tilemap.erase_cell(1, cell_pos)
+					tilemap.set_cell(0, cell_pos, 0, Constants.atlas.floor1)
 	pass
 
 func is_inside_padding(x: int, y: int, leaf: Leaf, padding: Vector4i) -> bool:
