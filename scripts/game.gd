a3d92579778da5c40ec3aed257a0c151fd39fd76
Hambloko <hambloko@gmail.com>
2024-07-10 18:08:25 -0500

    Player position fix - Turns out I just needed to use the map_to_local function to fix the issue with the player spawning at a weird offset.

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,6 +3,7 @@ extends Node2D
 var player_scene: PackedScene = preload("res://scenes/player.tscn")
 @onready var canvas_darkness: CanvasModulate = $Darkness
 @onready var dungeon_creator: DungeonCreator = $DungeonCreator
+@onready var tiles: TileMap = $Tiles
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -21,8 +22,6 @@ func spawn_player() -> void:
 	var starting_room: Leaf = rooms[rng.randi_range(0, rooms.size() - 1)]
 	var center: Vector2i = starting_room.get_center()
 
-	# For some reason, player position has to be offset by half the tilesize. I suspect the padding in dungeon creator is causing this issue
-	# but for now, just gonna use this temporary fix
-	player.position = Vector2i((center.x * Constants.TILE_SIZE) + 16, (center.y * Constants.TILE_SIZE) + 16)
+	player.position = tiles.map_to_local(Vector2i(center.x, center.y))
 	player.modulate = Color(0.478, 0, 0.478) 
 	add_child(player)
