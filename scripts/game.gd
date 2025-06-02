ae677342fb42e995993ebb3d43d9099c08d2186a
Joseph Cooper <hambloko@gmail.com>
2025-06-01 19:42:52 -0500

    Detached some superflous stuff from the main scene, renamed the project to Gremory

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,12 +2,11 @@ extends Node2D
 
 var player_scene: PackedScene = preload("res://scenes/player.tscn")
 @onready var canvas_darkness: CanvasModulate = $Darkness
-@onready var dungeon_creator: DungeonCreator = $DungeonCreator
-@onready var tiles: TileMap = $Tiles
+@onready var tiles: TileMap = $l1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	canvas_darkness.visible = true
+#	canvas_darkness.visible = true
 	spawn_player()
 	pass
 
@@ -18,10 +17,7 @@ func _process(delta: float) -> void:
 func spawn_player() -> void:
 	var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 	var player: Player = player_scene.instantiate()
-	var rooms: Array = dungeon_creator.root_node.get_leaves()
-	var starting_room: Leaf = rooms[rng.randi_range(0, rooms.size() - 1)]
-	var center: Vector2i = starting_room.get_center()
-
-	player.position = tiles.map_to_local(Vector2i(center.x, center.y))
+	
+	player.position = tiles.map_to_local(Vector2i(11, 6))
 	player.modulate = Color(0.478, 0, 0.478) 
 	add_child(player)
