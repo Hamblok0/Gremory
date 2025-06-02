f3236ba0b1b4bb557c964eb9819dcdee1a77a141
Joseph Cooper <hambloko@gmail.com>
2025-06-01 23:12:16 -0500

    Initial code to convert to tilemap layers. Map is screwed up but it renders. Will crash if you try to move

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 var player_scene: PackedScene = preload("res://scenes/player.tscn")
 @onready var canvas_darkness: CanvasModulate = $Darkness
-@onready var tiles: TileMap = $l1
+@onready var gmap: Map = $l1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,7 +17,7 @@ func _process(delta: float) -> void:
 func spawn_player() -> void:
 	var rng: RandomNumberGenerator = RandomNumberGenerator.new()
 	var player: Player = player_scene.instantiate()
-	
-	player.position = tiles.map_to_local(Vector2i(11, 6))
+	var floor: TileMapLayer = gmap.get_node("Floor")
+	player.position = floor.map_to_local(Vector2i(11, 6))
 	player.modulate = Color(0.478, 0, 0.478) 
 	add_child(player)
